4f0516c691ba242cb8e84129ba13a002137c11e1
Caleb Dell Laptop <omitted>
2020-08-15 22:10:37 -0500

    Started working on closures

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Closures:
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explain what UX design is?');
+  }
+} else if (job === 'teacher') {
+  return function(name) {
+  console.log('What subject do you teach ' + name + '?');
+  }
+} else {
+  return function(name) {
+    console.log('Hello ' + name + ', what do you do?');
+  }
+}
+}
+
+interviewQuestion('designer')('Caleb');
+
+
 // Function constructor
 
 // var Person = function (name, yearOfBirth, job) {
